e960fe55e623a1f2eabb74ed91cbb03092716c26
Jiro Kugiya <j.kugiya@gmail.com>
2018-02-24 15:58:09 +0900

    Tune up flow

--- src/main/scala/MyAkka.scala
+++ src/main/scala/MyAkka.scala
@@ -1,7 +1,7 @@
 import java.nio.file.Paths
 
 import akka.actor.ActorSystem
-import akka.stream.{ ActorMaterializer, OverflowStrategy }
+import akka.stream.{ ActorAttributes, ActorMaterializer, ClosedShape, OverflowStrategy }
 import akka.stream.alpakka.csv.scaladsl.{ CsvParsing, CsvToMap }
 import akka.stream.scaladsl._
 import akka.util.ByteString
@@ -13,7 +13,6 @@ object Main extends App {
   implicit val system = ActorSystem("MyAkkaActor")
   implicit val materializer = ActorMaterializer()
   // implicit val dipatcher = system.dispatcher
-  implicit val dispatcher = system.dispatchers.lookup("word-count")
   // val path = if (params.isEmpty) "../fukuokaex/test_12_000_000.csv" else params(0)
   //val path = "../fukuokaex/test_3_000_000.csv"
   val path = args(0)
@@ -23,31 +22,46 @@ object Main extends App {
   val grp_col = "lastname"
 
   val start = System.nanoTime()
-  source
-    .buffer(10000, OverflowStrategy.backpressure)
-    .via(CsvParsing.lineScanner())
-    .via(CsvToMap.withHeaders("firstname", "lastname", "gender", "birthday", "addr1", "addr2", "addr3", "state", "email", "zip", "tel", "attr", "regdate"))
-    .collect {
-      case rec if rec.get(grp_col).nonEmpty =>
-       rec(grp_col).utf8String
-    }
-    .groupBy(30, a => a(0)) // 頭一文字でグループ分け
-    .fold(acc_empty) { (acc: Map[String, Int], word: String) =>
-      val cnt = acc.getOrElse(word, 0)
-      acc.updated(word, cnt + 1)
-    }
-    .mergeSubstreams
-    .via(Flow[Map[String, Int]].fold(acc_empty) { (acc: Map[String, Int], rec: Map[String, Int]) =>
-      acc ++ rec.map { case (k, v) => k -> (v + acc.getOrElse(k, 0)) }
-    })
-    .runWith(Sink.last)
-    .onComplete(done ⇒ {
-      val nsec = (System.nanoTime() - start)
-      done.foreach { resultMap =>
-        resultMap.toSeq.sortWith(_._2 > _._2).take(10).foreach(println)
+  val bufferFlow = Flow[ByteString]
+      .buffer(10000, OverflowStrategy.backpressure)
+  val processLine = Flow[ByteString]
+      .via(CsvParsing.lineScanner())
+      .grouped(1000)
+      .mapAsync(8) { lines =>
+        val sink = Sink.fold[List[String], String](List.empty[String])((u, t) => t :: u)
+        RunnableGraph.fromGraph(GraphDSL.create(Source.fromFuture(Future.successful(lines.toList)), sink)(Keep.right) { implicit b => (in, out) =>
+            import GraphDSL.Implicits._
+            in ~> Flow[List[List[ByteString]]].mapConcat(identity) ~>
+                CsvToMap.withHeaders("firstname", "lastname", "gender", "birthday", "addr1", "addr2", "addr3", "state", "email", "zip", "tel", "attr", "regdate")
+                .collect {
+                  case rec if rec.get(grp_col).nonEmpty =>
+                    rec(grp_col).utf8String
+                } ~> out
+            ClosedShape
+        }).withAttributes(ActorAttributes.dispatcher("word-count")).run()
       }
-      println(nsec + "nsec")
-      println((nsec / 1000000) + "msec")
-      system.terminate()
-    })
+  val aggregate = Flow[String]
+      .groupBy(30, a => a(0)) // 頭一文字でグループ分け
+      .fold(acc_empty) { (acc: Map[String, Int], word: String) =>
+        val cnt = acc.getOrElse(word, 0)
+        acc.updated(word, cnt + 1)
+        }.mergeSubstreams
+      .via(Flow[Map[String, Int]].fold(acc_empty) { (acc: Map[String, Int], rec: Map[String, Int]) =>
+        acc ++ rec.map { case (k, v) => k -> (v + acc.getOrElse(k, 0)) }
+      })
+  val graph = RunnableGraph.fromGraph(GraphDSL.create(source, Sink.last[Map[String, Int]])(Keep.right) { implicit builder => (in, out) =>
+    import GraphDSL.Implicits._
+    in ~> bufferFlow ~> processLine.mapConcat(identity) ~> aggregate ~> out
+    ClosedShape
+  })
+  import system.dispatcher
+  graph.run().onComplete { done =>
+    val nsec = (System.nanoTime() - start)
+    done.foreach { resultMap =>
+      resultMap.toSeq.sortWith(_._2 > _._2).take(10).foreach(println)
+    }
+    println(nsec + "nsec")
+    println((nsec / 1000000) + "msec")
+    system.terminate()
+  }
 }
