6565b00f1fcda46568ceda8ff3931ce36f36e997
Jiro Kugiya <j.kugiya@gmail.com>
2018-02-24 14:58:11 +0900

    Improved algolithm

--- src/main/scala/MyAkka.scala
+++ src/main/scala/MyAkka.scala
@@ -1,37 +1,38 @@
 import java.nio.file.Paths
 
 import akka.actor.ActorSystem
-import akka.stream.ActorMaterializer
-import akka.stream.alpakka.csv.scaladsl.{CsvParsing, CsvToMap}
+import akka.stream.{ ActorMaterializer, OverflowStrategy }
+import akka.stream.alpakka.csv.scaladsl.{ CsvParsing, CsvToMap }
 import akka.stream.scaladsl._
 import akka.util.ByteString
+
+import scala.concurrent.Future
 
 
 object Main extends App {
   implicit val system = ActorSystem("MyAkkaActor")
   implicit val materializer = ActorMaterializer()
-  implicit val dipatcher = system.dispatcher
-
-
+  // implicit val dipatcher = system.dispatcher
+  implicit val dispatcher = system.dispatchers.lookup("word-count")
   // val path = if (params.isEmpty) "../fukuokaex/test_12_000_000.csv" else params(0)
   //val path = "../fukuokaex/test_3_000_000.csv"
   val path = args(0)
   val source = FileIO.fromPath(Paths.get(path))
 
   val acc_empty = Map.empty[String, Int]
-  var resultMap = Map.empty[String, Int]
   val grp_col = "lastname"
 
   val start = System.nanoTime()
   source
+    .buffer(10000, OverflowStrategy.backpressure)
     .via(CsvParsing.lineScanner())
     .via(CsvToMap.withHeaders("firstname", "lastname", "gender", "birthday", "addr1", "addr2", "addr3", "state", "email", "zip", "tel", "attr", "regdate"))
-    .map(_.map(r => (r._1, r._2.utf8String))) // Map( birthday ->'1950/01/20', email -> 'enpedasi@heaven.com' )
-    .filter(rec => rec.get(grp_col) != None)
-    .groupBy(30, r => r(grp_col)(0)) // 頭一文字でグループ分け
-    .async
-    .fold(acc_empty) { (acc: Map[String, Int], rec: Map[String, String]) =>
-      val word = rec(grp_col)
+    .collect {
+      case rec if rec.get(grp_col).nonEmpty =>
+       rec(grp_col).utf8String
+    }
+    .groupBy(30, a => a(0)) // 頭一文字でグループ分け
+    .fold(acc_empty) { (acc: Map[String, Int], word: String) =>
       val cnt = acc.getOrElse(word, 0)
       acc.updated(word, cnt + 1)
     }
@@ -39,13 +40,12 @@ object Main extends App {
     .via(Flow[Map[String, Int]].fold(acc_empty) { (acc: Map[String, Int], rec: Map[String, Int]) =>
       acc ++ rec.map { case (k, v) => k -> (v + acc.getOrElse(k, 0)) }
     })
-    .runWith(Sink.foreach(e => {
-      resultMap = e
-    }))
+    .runWith(Sink.last)
     .onComplete(done ⇒ {
       val nsec = (System.nanoTime() - start)
-      println(done)
-      resultMap.toSeq.sortWith(_._2 > _._2).take(10).foreach(println)
+      done.foreach { resultMap =>
+        resultMap.toSeq.sortWith(_._2 > _._2).take(10).foreach(println)
+      }
       println(nsec + "nsec")
       println((nsec / 1000000) + "msec")
       system.terminate()
