1b244d5ed3f560052afde30447789f1747128004
Jiro Kugiya <j.kugiya@gmail.com>
2018-02-28 06:51:27 +0900

    Uses StringUtils and AnyRefMap

--- src/main/scala/MyAkka.scala
+++ src/main/scala/MyAkka.scala
@@ -1,6 +1,9 @@
 import akka.actor.ActorSystem
 import akka.stream._
 import akka.stream.scaladsl._
+import org.apache.commons.lang3.StringUtils
+
+import scala.collection.mutable.AnyRefMap
 
 object Main extends App {
   implicit val system = ActorSystem("MyAkkaActor")
@@ -11,27 +14,21 @@ object Main extends App {
   val grp_col = "lastname"
   // 行の処理。CsaParsingは高級でやや重いようなので使わない
   val processCsv = Flow[String]
-      .map(_.split(","))
-      .filter(_.length > 2)
-      .collect {
-        case Array(_, lastname, _*) =>
-          lastname
-      }
+      .map(s => StringUtils.split(s, ",", 3)(1))
   // 集計処理
   val aggregate = Flow[String]
-      .groupBy(30, a => a(0)) // 頭一文字でグループ分け
-      .fold(Map.empty[String, Int]) { (acc: Map[String, Int], word: String) =>
-    val cnt = acc.getOrElse(word, 0)
-    acc.updated(word, cnt + 1)
+      .groupBy(30, s => Math.abs(s.hashCode)  % 30) // 頭一文字でグループ分け
+      .fold(AnyRefMap.empty[String, Int]) { (acc, word) =>
+    acc.updated(word, acc.getOrElse(word, 0) + 1)
   }.mergeSubstreams
-   .via(Flow[Map[String, Int]].fold(Map.empty[String, Int]) { (acc: Map[String, Int], rec: Map[String, Int]) =>
-     acc ++ rec.map { case (k, v) => k -> (v + acc.getOrElse(k, 0)) }
+   .via(Flow[AnyRefMap[String, Int]].fold(Map.empty[String, Int]) { (acc, rec) =>
+     acc ++ rec
    })
   val graph = RunnableGraph.fromGraph(GraphDSL.create(source, Sink.last[Map[String, Int]])(Keep.right) { implicit
   builder =>
     (in, out) =>
       import GraphDSL.Implicits._
-      in ~> Flow[String].buffer(10000, OverflowStrategy.backpressure) ~> processCsv ~> aggregate ~> out
+      in ~>  processCsv ~> aggregate ~> out
       ClosedShape
   })
 
