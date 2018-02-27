040e073ed38487b5b32067f7668ce35f04d35dbc
Jiro Kugiya <j.kugiya@gmail.com>
2018-02-27 22:49:06 +0900

    removed acc_empty

--- src/main/scala/MyAkka.scala
+++ src/main/scala/MyAkka.scala
@@ -8,7 +8,6 @@ object Main extends App {
   val path = args(0)
   val s = scala.io.Source.fromFile(path, "UTF-8")
   val source = Source.fromIterator(() => s.getLines())
-  val acc_empty = Map.empty[String, Int]
   val grp_col = "lastname"
   // 行の処理。CsaParsingは高級でやや重いようなので使わない
   val processCsv = Flow[String]
@@ -21,11 +20,11 @@ object Main extends App {
   // 集計処理
   val aggregate = Flow[String]
       .groupBy(30, a => a(0)) // 頭一文字でグループ分け
-      .fold(acc_empty) { (acc: Map[String, Int], word: String) =>
+      .fold(Map.empty[String, Int]) { (acc: Map[String, Int], word: String) =>
     val cnt = acc.getOrElse(word, 0)
     acc.updated(word, cnt + 1)
   }.mergeSubstreams
-   .via(Flow[Map[String, Int]].fold(acc_empty) { (acc: Map[String, Int], rec: Map[String, Int]) =>
+   .via(Flow[Map[String, Int]].fold(Map.empty[String, Int]) { (acc: Map[String, Int], rec: Map[String, Int]) =>
      acc ++ rec.map { case (k, v) => k -> (v + acc.getOrElse(k, 0)) }
    })
   val graph = RunnableGraph.fromGraph(GraphDSL.create(source, Sink.last[Map[String, Int]])(Keep.right) { implicit
